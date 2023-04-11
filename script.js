2f3dedcc0ad12dd1a06c47dcada8724be8d2ddff
cozyGarage <personal.trungnguyen@gmail.com>
2023-04-11 19:07:55 +0300

    improve js for modal toggle.from for-> foreach

--- script.js
+++ script.js
@@ -1,68 +1,47 @@
 'use strict';
-
-
-
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
-
 // sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
-
 // sidebar toggle functionality for mobile
 sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
-
-
-
 // testimonials variables
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
-
 // modal variable
 const modalImg = document.querySelector("[data-modal-img]");
 const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
-
 // modal toggle function
 const testimonialsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 }
-
 // add click event to all modal items
-for (let i = 0; i < testimonialsItem.length; i++) {
 
-  testimonialsItem[i].addEventListener("click", function () {
-
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
+testimonialsItem.forEach(item => {
+  item.addEventListener("click", function () {
+    const avatar = this.querySelector("[data-testimonials-avatar]");
+    modalImg.src = avatar.src;
+    modalImg.alt = avatar.alt;
     modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
     modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
-
     testimonialsModalFunc();
-
   });
-
-}
+});
 
 // add click event to modal close button
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
-
-
-
 // custom select variables
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
-
 select.addEventListener("click", function () { elementToggleFunc(this); });
-
 // add event in all select items
 for (let i = 0; i < selectItems.length; i++) {
   selectItems[i].addEventListener("click", function () {
@@ -156,7 +135,7 @@ form.addEventListener("submit", function (event) {
   // add event to return button
   returnBtn.addEventListener("click", function () {
     // redirect to homepage
-    window.location.href = "http://localhost:5500/index.html";
+    window.location.href = "http://www.cc.puv.fi/~e1500953";
   });
 });
 
